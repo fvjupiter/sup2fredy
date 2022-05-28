4ebd621bf5de84968df3e6dff1bf869beee99438
fvjupiter <schoof.frederik@gmail.com>
2022-05-28 21:10:13 +0200

    folder structure

--- pages/music/singles/[id].js
+++ pages/music/singles/[id].js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 export default function Single({ single }) {
-  console.log('single', single)
+  // console.log('single', single)
   return <div>Single</div>;
 }
 
-export async function getStaticPaths(){
-  const res = await fetch('http://localhost:1337/api/singles')
-  const { data } = await res.json()
-  const paths = data.map(post => ({
-          params: { id: String(post.id) }
-      }))
-  return {
-      paths,
-      fallback: false
-  }
-}
+// export async function getStaticPaths(){
+//   const res = await fetch('http://localhost:1337/api/singles')
+//   const { data } = await res.json()
+//   const paths = data.map(post => ({
+//           params: { id: String(post.id) }
+//       }))
+//   return {
+//       paths,
+//       fallback: false
+//   }
+// }
 
-export async function getStaticProps({ params }) {
-  const res = await fetch(`http://localhost:1337/api/singles/${params.id}/`) //ideally should be env.var //get by slug ()
-  const { data } = await res.json()
-  console.log('data in single', data)
-  return{
-      props: { single: data }
-  }
-}
+// export async function getStaticProps({ params }) {
+//   const res = await fetch(`http://localhost:1337/api/singles/${params.id}/`) //ideally should be env.var //get by slug ()
+//   const { data } = await res.json()
+//   console.log('data in single', data)
+//   return{
+//       props: { single: data }
+//   }
+// }
--- pages/music/singles/index.js
+++ pages/music/singles/index.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
-import PreviewCard from '../../../components/PreviewCard'
+import PreviewCard from '../../../components/cards/PreviewCard'
 import PageTitle from '../../../components/PageTitle'
 
 export default function Singles({ singles }) { 
-  console.log('singles: ', singles)
-console.log('audioURL: ', singles[0].attributes.audio.data[0].attributes.url)
+//   console.log('singles: ', singles)
+// console.log('audioURL: ', singles[0].attributes.audio.data[0].attributes.url)
 
-  let audio
-  const start = () => audio.play()
-  const stop = () => audio.pause()
-  useEffect(() => {
-    audio = new Audio(`http://localhost:1337${singles[0].attributes.audio.data[0].attributes.url}`)
-  }, [])
+//   let audio
+//   const start = () => audio.play()
+//   const stop = () => audio.pause()
+//   useEffect(() => {
+//     audio = new Audio(`http://localhost:1337${singles[0].attributes.audio.data[0].attributes.url}`)
+//   }, [])
   return <>
-  <PageTitle title='Singles'/>
+  {/* <PageTitle title='Singles'/>
   <button onClick={start}>Playyyyy</button><br/>
   <button onClick={stop}>stop</button>
     <div className='w-screen text-center flex justify-center'>
@@ -28,15 +28,15 @@ console.log('audioURL: ', singles[0].attributes.audio.data[0].attributes.url)
                 />
             ))}
         </div>
-    </div>
+    </div> */}
   </>
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:1337/api/singles?populate=audio") //ideally should be env.var
-  const { data } = await res.json()
-  console.log('singlesData ',data)
-  return{
-    props: { singles: data }
-  }
-}
+// export async function getStaticProps() {
+//   const res = await fetch("http://localhost:1337/api/singles?populate=audio") //ideally should be env.var
+//   const { data } = await res.json()
+//   console.log('singlesData ',data)
+//   return{
+//     props: { singles: data }
+//   }
+// }
