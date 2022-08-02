99c698401219a403890d93f4e7f54a9de600652d
fvjupiter <schoof.frederik@gmail.com>
2022-08-02 04:29:34 +0200

    dies das

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,7 +3,7 @@ import Social from './Social'
 
 export default function Footer({ setisImprint }) {
   return (
-    <div className='center h-80 w-screen relative pb-10 z-10 text-white bg-black'>
+    <div className='center h-80 w-screen relative pb-32 z-10 text-white bg-black'>
         <div className='sm:text-lg text-gray-400 text-center'>
             <Social />
             <div className='font-medium'>© 2022 All Rights Reserved.</div>
