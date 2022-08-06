317a5449b1b7fea1db8f9ade6b1de18ca75b70b6
fvjupiter <schoof.frederik@gmail.com>
2022-08-06 06:25:31 +0200

    double line break

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
     useEffect(() => {
         if(showInstaAlert && navigator.userAgent.includes("Instagram")) {
             setshowInstaAlert(false)
-            alert('Dear Instagram-User, please open the app in an external browser at\nsup2fredy.vercel.app\n💖\nSincerely, Fredy')
+            alert('Dear Instagram-User, please open the app in an external browser at\nsup2fredy.vercel.app\n\n💖\n\nSincerely, Fredy')
         }
     }, [asPath])
 
