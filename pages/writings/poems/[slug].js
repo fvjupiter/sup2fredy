24a71f3db9b26ae9901e7d8056507966f90e864f
Frederik Schoof <schoof.frederik@gmail.com>
2024-02-02 11:58:01 +0100

    text-orientation poems

--- pages/writings/poems/[slug].js
+++ pages/writings/poems/[slug].js
@@ -1,76 +1,91 @@
-import React, { useState, useEffect } from 'react'
-import { useRecoilState } from 'recoil'
-import { commentListState, slugListState } from '../../../lib/states'
-import { createClient } from 'contentful'
-import ContentCard from '../../../components/cards/ContentCard'
-import { getComments, writeComment } from '../../../firebase/action'
-import CommentCard from '../../../components/cards/CommentCard'
-import Comments from '../../../components/Comments'
+import React, { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { commentListState, slugListState } from "../../../lib/states";
+import { createClient } from "contentful";
+import ContentCard from "../../../components/cards/ContentCard";
+import { getComments, writeComment } from "../../../firebase/action";
+import CommentCard from "../../../components/cards/CommentCard";
+import Comments from "../../../components/Comments";
 
 export default function Poem({ poem, poemSlugList }) {
-
-    const [slugList, setslugList] = useRecoilState(slugListState)
-    useEffect(() => setslugList({
+  const [slugList, setslugList] = useRecoilState(slugListState);
+  useEffect(
+    () =>
+      setslugList({
         ...slugList,
-        poems: poemSlugList
-    }), [])
+        poems: poemSlugList,
+      }),
+    []
+  );
 
-    return <div className='mb-20'>
-        <ContentCard 
-            title={poem.fields.title} 
-            date={poem.fields.dateTitle} 
-            intro={poem.fields.intro} 
-            markdownContent={poem.fields.content} 
-            borderColor={'border-lime-400 ring-black/50'} 
-            bgColor={'bg-black'}
-            textOrientation='text-center'
-        />
+  return (
+    <div className="mb-20">
+      <ContentCard
+        title={poem.fields.title}
+        date={poem.fields.dateTitle}
+        intro={poem.fields.intro}
+        markdownContent={poem.fields.content}
+        borderColor={"border-lime-400 ring-black/50"}
+        bgColor={"bg-black"}
+        textOrientation={
+          poem.fields.textOriention == "text-start"
+            ? "text-left"
+            : poem.fields.textOriention == "text-end"
+            ? "text-right"
+            : "text-center"
+        }
+      />
 
-        <Comments border={`border-lime-400 ring-black/50`} bg_success={`bg-lime-400`} bg_success_hover={`hover:bg-lime-200`} ring={`ring-lime-200`}/>
-
+      <Comments
+        border={`border-lime-400 ring-black/50`}
+        bg_success={`bg-lime-400`}
+        bg_success_hover={`hover:bg-lime-200`}
+        ring={`ring-lime-200`}
+      />
     </div>
-} 
+  );
+}
 
 const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-})
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: "poem" });
 
-    const res = await client.getEntries({ content_type: 'poem' })
-
-    const paths = res.items.map(item => {
-        return {
-            params: { slug: item.fields.slug }
-        }
-    })
-
+  const paths = res.items.map((item) => {
     return {
-        paths,
-        fallback: false
-    }
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "poem",
+    "fields.slug": params.slug,
+  });
 
-    const { items } = await client.getEntries({ 
-        content_type: 'poem',
-        'fields.slug': params.slug,
-    
-    })
+  const res2 = await client.getEntries({
+    content_type: "poem",
+    order: "fields.indexFloat",
+  });
 
-    const res2 = await client.getEntries({ content_type: 'poem', order: 'fields.indexFloat' })
+  const poemSlugList = [];
+  for (let i = 0; i < res2.items.length; i++) {
+    poemSlugList.push(res2.items[i].fields.slug);
+  }
 
-    const poemSlugList = []
-    for (let i = 0; i < res2.items.length; i++) {
-        poemSlugList.push(res2.items[i].fields.slug)
-    }
-
-    return {
-        props: {
-            poem: items[0],
-            poemSlugList: poemSlugList
-        }
-    }
+  return {
+    props: {
+      poem: items[0],
+      poemSlugList: poemSlugList,
+    },
+  };
 }
